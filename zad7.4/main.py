b455c3064ca151b252a6438b2556755b62eeac7c
Marcin Sarna <sarna077@gmail.com>
2023-03-04 16:37:28 +0100

    Add some corrects

--- zad7.4/main.py
+++ zad7.4/main.py
@@ -1,6 +1,8 @@
 from faker import Faker
-fake = Faker('pl_PL')
+
+fake = Faker("pl_PL")
 import random
+import black
 
 
 class Movie:
@@ -9,33 +11,54 @@ class Movie:
         self.year = year
         self.species = species
         self.number_of_plays = number_of_plays
+
     def play(self):
-        self.number_of_plays +=1
+        self.number_of_plays += 1
+
     def __str__(self):
         return f'"{self.title} ({self.year})'
+
     def __repr__(self):
-        return f'{self.title} {self.year} {self.species}{self.number_of_plays}'
+        return f"{self.title} {self.year} {self.species}{self.number_of_plays}"
+
 
 class Series(Movie):
     def __init__(self, episode, season, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.episode = episode
         self.season = season
+
     def __str__(self):
         return f'"{self.title} S{self.season}E{self.episode}"'
+
     def __repr__(self):
-        return f'{self.title} {self.year} {self.species} {self.number_of_plays} {self.season} {self.episode}'
+        return f"{self.title} {self.year} {self.species} {self.number_of_plays} {self.season} {self.episode}"
 
 
+def get_class(zmienna, new_list, search_type):
+    for i in zmienna:
+        if type(i) == search_type:
+            new_list.append(i)
+    lista1 = sorted(new_list, key=lambda x: x.title)
+    return lista1
 
 
+def get_movies(x):
+    get_class(x, lista_movie, Movie)
+    return lista_movie
 
-def get_movies(lista):
+
+def get_series(x):
+    get_class(x, lista_series, Series)
+    return lista_series
+
+
+"""    
     for i in lista:
         if type(i) == Movie:
             lista_movie.append(i)
     lista1 = sorted(lista_movie, key=lambda movie: movie.title)
-    return(lista1)
+    return lista1
 
 
 def get_series(lista):
@@ -43,7 +66,8 @@ def get_series(lista):
         if type(i) == Series:
             lista_series.append(i)
     lista2 = sorted(lista_series, key=lambda series: series.title)
-    return(lista2)
+    return lista2
+"""
 
 
 def search(lista):
@@ -62,7 +86,7 @@ def generate_views(lista):
     random.choice(lista).number_of_plays += random.choice(range(1, 101))
 
 
-def generate_views_10():
+def generate_views_10(lista):
     for i in range(11):
         generate_views(lista)
 
@@ -70,30 +94,44 @@ def generate_views_10():
 def top_titles(lista):
     amount = int(input("Podaj ilość tytułów:"))
     lista3 = sorted(lista, key=lambda movie: movie.number_of_plays, reverse=True)
-    print(lista3[0:amount])
+    return lista3[0:amount]
 
 
 if __name__ == "__main__":
-    game_of_thrones = Series(title="Game of Thrones", year=2010, species="fantasy", number_of_plays=10, episode=2,
-                             season=2)
-    prison_break = Series(title="Prison Break", year=2004, species="criminal", number_of_plays=15, episode=10,
-                          season=13)
-    harry_potter = Movie(title="Harry Potter", year=2005, species="fantasy", number_of_plays=100)
-    lord_of_rings = Movie(title="The Lord of the Rings", year=2000, species="fantasy", number_of_plays=150)
+    game_of_thrones = Series(
+        title="Game of Thrones",
+        year=2010,
+        species="fantasy",
+        number_of_plays=10,
+        episode=2,
+        season=2,
+    )
+    prison_break = Series(
+        title="Prison Break",
+        year=2004,
+        species="criminal",
+        number_of_plays=15,
+        episode=10,
+        season=13,
+    )
+    harry_potter = Movie(
+        title="Harry Potter", year=2005, species="fantasy", number_of_plays=100
+    )
+    lord_of_rings = Movie(
+        title="The Lord of the Rings", year=2000, species="fantasy", number_of_plays=150
+    )
+
     lista = [game_of_thrones, prison_break, harry_potter, lord_of_rings]
     lista_movie = []
     lista_series = []
-
-print(lista)
-
-
-generate_views(lista)
-generate_views_10()
-print(get_movies(lista))
-print(get_series(lista))
-print(game_of_thrones.number_of_plays)
-print(prison_break.number_of_plays)
-print(lord_of_rings.number_of_plays)
-print(harry_potter.number_of_plays)
-search(lista)
-top_titles(lista)
+    print(lista)
+    generate_views(lista)
+    generate_views_10(lista)
+    print(get_movies(lista))
+    print(get_series(lista))
+    print(game_of_thrones.number_of_plays)
+    print(prison_break.number_of_plays)
+    print(lord_of_rings.number_of_plays)
+    print(harry_potter.number_of_plays)
+    search(lista)
+    print(top_titles(lista))
